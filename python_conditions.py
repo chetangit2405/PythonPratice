c4de523dfd0d0ece0cc9750b7177b117dbb2f09f
ChetanKumar <chetan.chinna@gmail.com>
2025-05-06 16:23:48 +0530

    Commit Python varaibles, conditions, loops and datatypes

---
+++ python_conditions.py
@@ -0,0 +1,6 @@
+age = input("Enter your age:")
+age = int(age)
+if age >= 18:
+    print("You are eligible for driving license")
+else:
+    print("You are not eligible for driving license")
---
+++ python_datatype.py
@@ -0,0 +1,5 @@
+birth_year = input("Enter your birth year:")
+birth_year = int(birth_year)
+age = 2025 - birth_year
+
+print(f"You are {age} years old.")
