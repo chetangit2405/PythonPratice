5a961739dd0ee1afc16c2970e1fd576e962090f3
ChetanKumar <chetan.chinna@gmail.com>
2025-05-10 16:59:27 +0530

    Dictionaries inside List

---
+++ python_Listdictionaries.py
@@ -0,0 +1,19 @@
+def salary(employees, typeOperation):
+        operType = typeOperation
+
+        for employee in employees:
+                if operType == "compare":
+                        if int(employee["salary"]) > 50000:
+                                print(f"Employee {employee['name']} salary is {employee['salary']}. Salary is morethan 50000.")
+                else:
+                        print(f"Employee {employee['name']} \t| Position {employee['position']}.", end='\n')
+                        
+
+employees = [{"name": "Chetan","position":"SE","salary":100000},{"name": "Mamatha","position":"Teacher","salary":150000},{"name": "Sony","position":"staff","salary":10000}]
+
+employees.append({"name": "Chinna","position":"Marketing","salary":90000})
+
+salary(employees, "compare")
+print("_______________________________" + '\n')
+salary(employees, "listEmployee")
+
